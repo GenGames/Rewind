15d8dcbc8efbcd79b3b954817e4c83a832f4e631
GenGames <48166284+GenGames@users.noreply.github.com>
2020-08-02 19:04:31 -0500

    2019 switch

---
+++ Rewind 2019/Assets/Scripts/TransitionScripts/TransitionController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+using UnityStandardAssets._2D;
+
+public class TransitionController : MonoBehaviour
+{
+    public KeyCode initiationKey = KeyCode.Q;
+    public Camera camera2d;
+    public Camera camera3d;
+    public ThirdPersonUserControl controller3d;
+    public ThirdPersonCharacter movement3d;
+    public FireProjectile fireProjectile;
+    public Platformer2DUserControl controller2d;
+    public PlatformerCharacter2D movement2d;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(initiationKey))
+        {
+            TransitionToggle();
+        }
+    }
+
+    public void TransitionToggle()
+    {
+        print("toggling transition");
+    }
+}
---
+++ Rewind 2019/Assets/_2DCharacterController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _2DCharacterController : MonoBehaviour
+{
+    public float jumpHeight;
+    public float speed;
+    public Animator animator;
+    private bool isGrounded;
+    public Transform testCeiling;
+    public Transform testGround;
+
+    private void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            Move();
+        }
+        if (Input.GetAxis("Jump") != 0)
+        {
+            Jump();
+        }
+        if (Input.GetAxis("Vertical") < 0 )
+        {
+            Drop();
+        }
+    }
+
+    public void Move()
+    {
+        print("moving");
+        transform.Translate(-Input.GetAxis("Horizontal") * new Vector3(0, 0, 1) * speed);
+    }
+
+    public void Jump()
+    {
+        print("jumping!");
+    }
+
+    public void Drop()
+    {
+        print("dropping");
+    }
+
+
+
+
+    // Needs to.... Move, Jump, detect collisions
+        // Move
+        // - a/d forward/backward
+        // - trigger animation when moving/falling
+        // - s for dropping
+        // - character flipping
+
+        // Jump
+        // - Space to jump
+        // - hold on for jump control
+
+        // Collision detection
+        // - if jumping up, disable collider so that you can jump over it
+        // - reenable collider when falling down so that you can rest on it
+        // - disable collider below when pressing S so that you fall.
+
+}
