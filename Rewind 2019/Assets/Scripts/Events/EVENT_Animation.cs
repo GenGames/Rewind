4845bd0690649a43d82b9bb37eaa1275fc85510c
GenGames <48166284+GenGames@users.noreply.github.com>
2020-08-05 14:26:32 -0500

    EventTriggers

---
+++ Rewind 2019/Assets/Scripts/Events/EVENT_Animation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EVENT_Animation : EventTrigger
+{
+    public Animator[] animations;
+    public string animatorTriggerString;
+    public override void EventTriggered()
+    {
+        print("event triggered");
+        foreach (Animator animator in animations)
+        {
+            print("setting animator trigger of " +animator.gameObject.name +" to " + animatorTriggerString);
+            animator.SetTrigger(animatorTriggerString);
+        }
+    }
+}
--- Rewind 2019/Assets/Scripts/Events/EventTrigger.cs
+++ Rewind 2019/Assets/Scripts/Events/EventTrigger.cs
@@ -6,16 +6,23 @@ using UnityEngine;
 public class EventTrigger : MonoBehaviour
 {
     private BoxCollider eventRegion;
+    public string eventTriggerTag = "Player";
+    public GameObject eventTriggerObject;
 
     // Start is called before the first frame update
     void Start()
     {
+        eventRegion = GetComponent<BoxCollider>();
         eventRegion.isTrigger = true;
+        if (eventTriggerTag == "")
+        {
+            eventTriggerTag = "Untagged";
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject == eventTriggerObject || other.gameObject.CompareTag(eventTriggerTag))
         {
             EventTriggered();
         }
